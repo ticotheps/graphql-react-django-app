813139dc2ffb5cafc7fd4fe67d96f63dc3182dd2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-02 02:08:30 -0400

    Installs dependencies using the 'pip install django graphene-django django-filter djangorestframework' command in the terminal

---
+++ back-end/.venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ticothep/my_projects/graphql-react-django-app/back-end/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
