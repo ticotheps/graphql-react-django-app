18a69eaed347ab780c131dbcd20f3a2935804771
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-04-03 02:56:04 -0400

    Creates a schema

---
+++ back-end/gql/schema.py
@@ -0,0 +1,15 @@
+from graphene import Argument, Field, ID, ObjectType, Schema
+from graphene_django import DjangoConnectionField
+from resources.models import Resource
+from .resources.types import ResourceType
+
+
+class Query(ObjectType):
+    resources = DjangoConnectionField(ResourceType)
+    resource = Field(ResourceType, id=Argument(ID, required=True))
+    
+    def resolve_resources(root, info, **kwargs):
+        return Resource.objects.all()
+    
+    def resolve_resource(root, info, **kwargs):
+        return Resource.objects.get(id=kwargs.get('id'))
